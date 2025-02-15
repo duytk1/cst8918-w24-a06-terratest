effea707eaf35e96e709eed5d84c99d912434646
D <duytk.job@gmail.com>
2025-02-15 00:50:10 -0500

    update for lab6

---
+++ test/azure_webserver_test.go
@@ -0,0 +1,32 @@
+package test
+
+import (
+	"testing"
+
+	"github.com/gruntwork-io/terratest/modules/azure"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestAzureLinuxVMCreation(t *testing.T) {
+	t.Parallel()
+
+	subscriptionID := "your-azure-subscription-id"
+	resourceGroupName := "your-resource-group"
+	vmName := "your-vm-name"
+
+	// Verify VM exists
+	vm, err := azure.GetVirtualMachineE(subscriptionID, resourceGroupName, vmName)
+	require.NoError(t, err)
+	assert.NotNil(t, vm)
+
+	// Confirm NIC exists and is connected to the VM
+	nicName := vm.NetworkProfile.NetworkInterfaces[0].ID
+	nic, err := azure.GetNetworkInterfaceE(subscriptionID, resourceGroupName, nicName)
+	require.NoError(t, err)
+	assert.NotNil(t, nic)
+
+	// Confirm the VM is running the correct Ubuntu version
+	expectedOS := "Ubuntu"
+	assert.Contains(t, *vm.StorageProfile.ImageReference.Offer, expectedOS, "VM is not running the expected OS")
+}
